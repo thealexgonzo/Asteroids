d6c992b56f30153ad43c4ff40736123822605c9f
Alejandro González <alexgonzorod@hotmail.com>
2025-07-29 12:07:12 +0100

    created player object

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #This allows us to use code from the open-source pygame library throughout this file
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -9,12 +10,19 @@ def main():
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
